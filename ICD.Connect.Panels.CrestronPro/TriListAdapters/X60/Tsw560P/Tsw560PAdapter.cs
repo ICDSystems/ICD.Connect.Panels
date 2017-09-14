f262d194639b4429fcc2ba0dec842877e84d849d
Chris Cameron <chris.cameron@wedoresi.com>
2017-09-14 12:18:08 -0400

    Initial commit of TSW560P adapter

---
+++ ICD.Connect.Panels.CrestronPro/TriListAdapters/X60/Tsw560P/Tsw560PAdapter.cs
@@ -0,0 +1,26 @@
+﻿#if SIMPLSHARP
+#endif
+using Crestron.SimplSharpPro;
+
+namespace ICD.Connect.Panels.CrestronPro.TriListAdapters.X60.Tsw560P
+{
+#if SIMPLSHARP
+	public sealed class Tsw560PAdapter : AbstractTswX60BaseClassAdapter<Crestron.SimplSharpPro.UI.Tsw560P, Tsw560PAdapterSettings>
+	{
+		/// <summary>
+		/// Creates an instance of the wrapped trilist.
+		/// </summary>
+		/// <param name="ipid"></param>
+		/// <param name="controlSystem"></param>
+		/// <returns></returns>
+		protected override Crestron.SimplSharpPro.UI.Tsw560P InstantiateTriList(byte ipid, CrestronControlSystem controlSystem)
+		{
+			return new Crestron.SimplSharpPro.UI.Tsw560P(ipid, controlSystem);
+		}
+	}
+#else
+    public sealed class Tsw760Adapter : AbstractTriListAdapter<Tsw760AdapterSettings>
+    {
+    }
+#endif
+}
---
+++ ICD.Connect.Panels.CrestronPro/TriListAdapters/X60/Tsw560P/Tsw560PAdapterSettings.cs
@@ -0,0 +1,34 @@
+using System;
+using ICD.Common.Properties;
+using ICD.Connect.Settings.Attributes.Factories;
+
+namespace ICD.Connect.Panels.CrestronPro.TriListAdapters.X60.Tsw560P
+{
+	public sealed class Tsw560PAdapterSettings : AbstractTswX60BaseClassAdapterSettings
+	{
+		private const string FACTORY_NAME = "Tsw560P";
+
+		/// <summary>
+		/// Gets the originator factory name.
+		/// </summary>
+		public override string FactoryName { get { return FACTORY_NAME; } }
+
+		/// <summary>
+		/// Gets the type of the originator for this settings instance.
+		/// </summary>
+		public override Type OriginatorType { get { return typeof(Tsw560PAdapter); } }
+
+		/// <summary>
+		/// Loads the settings from XML.
+		/// </summary>
+		/// <param name="xml"></param>
+		/// <returns></returns>
+		[PublicAPI, XmlPanelSettingsFactoryMethod(FACTORY_NAME)]
+		public static Tsw560PAdapterSettings FromXml(string xml)
+		{
+			Tsw560PAdapterSettings output = new Tsw560PAdapterSettings();
+			ParseXml(output, xml);
+			return output;
+		}
+	}
+}
