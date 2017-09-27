560921f62c64be20dd3b577c1538e9b505d5dc7c
Chris Cameron <chris.cameron@wedoresi.com>
2017-09-27 15:18:51 -0400

    Supports CrestronApp panels

---
+++ ICD.Connect.Panels.CrestronPro/TriListAdapters/CrestronApp/CrestronAppAdapter.cs
@@ -0,0 +1,18 @@
+﻿using Crestron.SimplSharpPro;
+
+namespace ICD.Connect.Panels.CrestronPro.TriListAdapters.CrestronApp
+{
+	public sealed class CrestronAppAdapter : AbstractTriListAdapter<Crestron.SimplSharpPro.UI.CrestronApp, CrestronAppAdapterSettings>
+	{
+		/// <summary>
+		/// Creates an instance of the wrapped trilist.
+		/// </summary>
+		/// <param name="ipid"></param>
+		/// <param name="controlSystem"></param>
+		/// <returns></returns>
+		protected override Crestron.SimplSharpPro.UI.CrestronApp InstantiateTriList(byte ipid, CrestronControlSystem controlSystem)
+		{
+			return new Crestron.SimplSharpPro.UI.CrestronApp(ipid, controlSystem);
+		}
+	}
+}
---
+++ ICD.Connect.Panels.CrestronPro/TriListAdapters/CrestronApp/CrestronAppAdapterSettings.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using ICD.Common.Properties;
+using ICD.Connect.Settings.Attributes.Factories;
+
+namespace ICD.Connect.Panels.CrestronPro.TriListAdapters.CrestronApp
+{
+	public sealed class CrestronAppAdapterSettings : AbstractTriListAdapterSettings
+	{
+		private const string FACTORY_NAME = "CrestronApp";
+
+		/// <summary>
+		/// Gets the originator factory name.
+		/// </summary>
+		public override string FactoryName { get { return FACTORY_NAME; } }
+
+		/// <summary>
+		/// Gets the type of the originator for this settings instance.
+		/// </summary>
+		public override Type OriginatorType { get { return typeof(CrestronAppAdapter); } }
+
+		/// <summary>
+		/// Loads the settings from XML.
+		/// </summary>
+		/// <param name="xml"></param>
+		/// <returns></returns>
+		[PublicAPI, XmlPanelSettingsFactoryMethod(FACTORY_NAME)]
+		public static CrestronAppAdapterSettings FromXml(string xml)
+		{
+			CrestronAppAdapterSettings output = new CrestronAppAdapterSettings();
+			ParseXml(output, xml);
+			return output;
+		}
+	}
+}
