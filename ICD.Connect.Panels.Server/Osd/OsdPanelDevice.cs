f5282b171574a6009e151538836c5464bf400ab8
Chris Cameron <chris.cameron@wedoresi.com>
2018-02-10 09:28:37 -0500

    Initial commit of OSD device

---
+++ ICD.Connect.Panels.Server/Osd/OsdPanelDevice.cs
@@ -0,0 +1,19 @@
+﻿using ICD.Connect.Routing.Connections;
+using ICD.Connect.Routing.Mock.Source;
+
+namespace ICD.Connect.Panels.Server.Osd
+{
+    public sealed class OsdPanelDevice : AbstractPanelServerDevice<OsdPanelDeviceSettings>
+    {
+		/// <summary>
+		/// Constructor.
+		/// </summary>
+		public OsdPanelDevice()
+		{
+			MockRouteSourceControl routingControl = new MockRouteSourceControl(this, 0);
+			routingControl.SetActiveTransmissionState(1, eConnectionType.Video, true);
+
+			Controls.Add(routingControl);
+		}
+	}
+}
---
+++ ICD.Connect.Panels.Server/Osd/OsdPanelDeviceSettings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using ICD.Connect.Settings.Attributes;
+
+namespace ICD.Connect.Panels.Server.Osd
+{
+	[KrangSettings(FACTORY_NAME)]
+	public sealed class OsdPanelDeviceSettings : AbstractPanelServerDeviceSettings
+	{
+		private const string FACTORY_NAME = "OsdPanel";
+
+		/// <summary>
+		/// Gets the originator factory name.
+		/// </summary>
+		public override string FactoryName { get { return FACTORY_NAME; } }
+
+		public override Type OriginatorType { get { return typeof(OsdPanelDevice); } }
+	}
+}
