4dd5e7472ba86b5018a174976a3ee79ac8f2e4fe
Jack Kanarish <jack.kanarish@wedoresi.com>
2018-04-02 15:00:35 -0400

    new versioning commit

--- ICD.Connect.Panels.Server/Properties/AssemblyInfo.cs
+++ ICD.Connect.Panels.Server/Properties/AssemblyInfo.cs
@@ -1,7 +1,7 @@
-﻿using System.Reflection;
+using System.Reflection;
 
 [assembly: AssemblyTitle("ICD.Connect.Panels.Server")]
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Connect.Panels.Server")]
 [assembly: AssemblyCopyright("Copyright © ICD Systems 2018")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("3.0.0.0")]
