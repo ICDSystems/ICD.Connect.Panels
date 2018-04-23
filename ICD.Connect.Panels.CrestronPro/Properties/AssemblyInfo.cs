c0e6fe3874fd3b3b63c893ae4f23f56eb1205cfe
Chris Cameron <chris.cameron@wedoresi.com>
2018-04-23 11:40:47 -0400

    chore: Updating assembly versions and changelog

--- ICD.Connect.Panels.CrestronPro/Properties/AssemblyInfo.cs
+++ ICD.Connect.Panels.CrestronPro/Properties/AssemblyInfo.cs
@@ -4,4 +4,4 @@ using System.Reflection;
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Common.Panels.CrestronPro")]
 [assembly: AssemblyCopyright("Copyright © ICD Systems 2018")]
-[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyVersion("4.0.0.0")]
