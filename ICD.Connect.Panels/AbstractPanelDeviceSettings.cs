c611e17d04e79b34fbf93c3976ebd40ee869f47d
Chris Cameron <chris.cameron@wedoresi.com>
2018-05-04 10:00:37 -0400

    refactor: Settings element provided by parent

--- ICD.Connect.Panels/AbstractPanelDeviceSettings.cs
+++ ICD.Connect.Panels/AbstractPanelDeviceSettings.cs
@@ -7,11 +7,5 @@ namespace ICD.Connect.Panels
 	/// </summary>
 	public abstract class AbstractPanelDeviceSettings : AbstractDeviceBaseSettings, IPanelDeviceSettings
 	{
-		private const string PANEL_ELEMENT = "Panel";
-
-		/// <summary>
-		/// Gets the xml element.
-		/// </summary>
-		protected override string Element { get { return PANEL_ELEMENT; } }
 	}
 }
