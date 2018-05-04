ff6c2a9e6f45124fa3a9d98c3877f159b44c84f7
Chris Cameron <chris.cameron@wedoresi.com>
2018-05-04 17:15:27 -0400

    refactor: Tidying

--- ICD.Connect.Panels.Server/Osd/OsdPanelDeviceSettings.cs
+++ ICD.Connect.Panels.Server/Osd/OsdPanelDeviceSettings.cs
@@ -1,18 +1,9 @@
-﻿using System;
-using ICD.Connect.Settings.Attributes;
+﻿using ICD.Connect.Settings.Attributes;
 
 namespace ICD.Connect.Panels.Server.Osd
 {
-	[KrangSettings(FACTORY_NAME)]
+	[KrangSettings("OsdPanel", typeof(OsdPanelDevice))]
 	public sealed class OsdPanelDeviceSettings : AbstractPanelServerDeviceSettings
 	{
-		private const string FACTORY_NAME = "OsdPanel";
-
-		/// <summary>
-		/// Gets the originator factory name.
-		/// </summary>
-		public override string FactoryName { get { return FACTORY_NAME; } }
-
-		public override Type OriginatorType { get { return typeof(OsdPanelDevice); } }
 	}
 }
