02ccedd7497090d0d274b0390bca1b2707793820
Chris Cameron <chris.cameron@wedoresi.com>
2018-05-07 14:12:22 -0400

    refactor: Resolving warnings

--- ICD.Connect.Panels.CrestronPro/TriListAdapters/X60/Tsw1060/Tsw1060AdapterSettings.cs
+++ ICD.Connect.Panels.CrestronPro/TriListAdapters/X60/Tsw1060/Tsw1060AdapterSettings.cs
@@ -1,21 +1,9 @@
-using System;
 using ICD.Connect.Settings.Attributes;
 
 namespace ICD.Connect.Panels.CrestronPro.TriListAdapters.X60.Tsw1060
 {
-	[KrangSettings(FACTORY_NAME)]
+	[KrangSettings("Tsw1060", typeof(Tsw1060Adapter))]
 	public sealed class Tsw1060AdapterSettings : AbstractTswX60BaseClassAdapterSettings
 	{
-		private const string FACTORY_NAME = "Tsw1060";
-
-		/// <summary>
-		/// Gets the originator factory name.
-		/// </summary>
-		public override string FactoryName { get { return FACTORY_NAME; } }
-
-		/// <summary>
-		/// Gets the type of the originator for this settings instance.
-		/// </summary>
-		public override Type OriginatorType { get { return typeof(Tsw1060Adapter); } }
 	}
 }
--- ICD.Connect.Panels.Mock/MockPanelDeviceSettings.cs
+++ ICD.Connect.Panels.Mock/MockPanelDeviceSettings.cs
@@ -1,18 +1,9 @@
-using System;
 using ICD.Connect.Settings.Attributes;
 
 namespace ICD.Connect.Panels.Mock
 {
-	[KrangSettings(FACTORY_NAME)]
+	[KrangSettings("MockPanel", typeof(MockPanelDevice))]
 	public sealed class MockPanelDeviceSettings : AbstractPanelDeviceSettings
 	{
-		private const string FACTORY_NAME = "MockPanel";
-
-		/// <summary>
-		/// Gets the originator factory name.
-		/// </summary>
-		public override string FactoryName { get { return FACTORY_NAME; } }
-
-		public override Type OriginatorType { get { return typeof(MockPanelDevice); } }
 	}
 }
