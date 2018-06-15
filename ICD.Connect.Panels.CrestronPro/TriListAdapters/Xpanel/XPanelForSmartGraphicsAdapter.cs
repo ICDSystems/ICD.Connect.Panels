ba95657a403541d12b1771b6b7a7aa569f8ba794
Chris Cameron <chris.cameron@wedoresi.com>
2018-06-15 12:01:53 -0400

    feat: Adding Xpanel adapter

---
+++ ICD.Connect.Panels.CrestronPro/TriListAdapters/Xpanel/XPanelForSmartGraphicsAdapter.cs
@@ -0,0 +1,27 @@
+﻿#if SIMPLSHARP
+using Crestron.SimplSharpPro;
+using Crestron.SimplSharpPro.UI;
+#endif
+
+namespace ICD.Connect.Panels.CrestronPro.TriListAdapters.XPanel
+{
+#if SIMPLSHARP
+	public sealed class XpanelForSmartGraphicsAdapter : AbstractTriListAdapter<XpanelForSmartGraphics, XpanelForSmartGraphicsAdapterSettings>
+#else
+	public sealed class XpanelForSmartGraphicsAdapter : AbstractTriListAdapter<XpanelForSmartGraphicsAdapterSettings>
+#endif
+	{
+#if SIMPLSHARP
+		/// <summary>
+		/// Creates an instance of the wrapped trilist.
+		/// </summary>
+		/// <param name="ipid"></param>
+		/// <param name="controlSystem"></param>
+		/// <returns></returns>
+		protected override XpanelForSmartGraphics InstantiateTriList(byte ipid, CrestronControlSystem controlSystem)
+		{
+			return new XpanelForSmartGraphics(ipid, controlSystem);
+		}
+#endif
+	}
+}
