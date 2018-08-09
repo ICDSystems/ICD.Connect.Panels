2d3e45ed76cf60677450a109602a5b78b4f16be7
Chris Cameron <chris.cameron@wedoresi.com>
2018-08-08 20:59:13 -0400

    fix: Better thread safety

--- ICD.Connect.Panels.Mock/AbstractMockSigCollection.cs
+++ ICD.Connect.Panels.Mock/AbstractMockSigCollection.cs
@@ -20,7 +20,7 @@ namespace ICD.Connect.Panels.Mock
 		/// Returns the number of sig objects in the collection.
 		/// </summary>
 		[PublicAPI]
-		public int NumberOfSigs { get { return m_SigCache.Count; } }
+		public int NumberOfSigs { get { return m_SigCacheSection.Execute(() => m_SigCache.Count); } }
 
 		/// <summary>
 		/// Get the sig with the specified number.
@@ -61,14 +61,14 @@ namespace ICD.Connect.Panels.Mock
 		[PublicAPI]
 		public void AddSig(ushort number, T sig)
 		{
-			m_SigCache[number] = sig;
+			m_SigCacheSection.Execute(() => m_SigCache[number] = sig);
 		}
 
 		protected abstract T InstantiateSig(uint sigNumber);
 
 		public IEnumerator<T> GetEnumerator()
 		{
-			return m_SigCacheSection.Execute(() => m_SigCache.Values).ToList().GetEnumerator();
+			return m_SigCacheSection.Execute(() => m_SigCache.Values.ToList()).GetEnumerator();
 		}
 
 		IEnumerator IEnumerable.GetEnumerator()
