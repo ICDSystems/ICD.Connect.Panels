7ad64c3ce15420541f56c73a722178bf42c835a3
Chris Cameron <chris.cameron@wedoresi.com>
2018-08-29 14:07:43 -0400

    feat: Begin implementing panel controls

---
+++ ICD.Connect.Panels/Controls/AbstractSigControl.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using ICD.Connect.Devices;
+using ICD.Connect.Devices.Controls;
+using ICD.Connect.Panels.EventArguments;
+using ICD.Connect.Protocol.Sigs;
+
+namespace ICD.Connect.Panels.Controls
+{
+	public abstract class AbstractSigControl<TParent> : AbstractDeviceControl<TParent>, ISigControl
+		where TParent : IDeviceBase
+	{
+		/// <summary>
+		/// Raised when the user interacts with the panel.
+		/// </summary>
+		public abstract event EventHandler<SigInfoEventArgs> OnAnyOutput;
+
+		/// <summary>
+		/// Constructor.
+		/// </summary>
+		/// <param name="parent"></param>
+		/// <param name="id"></param>
+		protected AbstractSigControl(TParent parent, int id)
+			: base(parent, id)
+		{
+		}
+
+		/// <summary>
+		/// Gets the time that the user last interacted with the panel.
+		/// </summary>
+		public abstract DateTime? LastOutput { get; }
+
+		/// <summary>
+		/// Clears the assigned input sig values.
+		/// </summary>
+		public abstract void Clear();
+
+		/// <summary>
+		/// Registers the callback for output sig change events.
+		/// </summary>
+		/// <param name="number"></param>
+		/// <param name="type"></param>
+		/// <param name="callback"></param>
+		public abstract void RegisterOutputSigChangeCallback(uint number, eSigType type,
+		                                                     Action<SigCallbackManager, SigInfoEventArgs> callback);
+
+		/// <summary>
+		/// Unregisters the callback for output sig change events.
+		/// </summary>
+		/// <param name="number"></param>
+		/// <param name="type"></param>
+		/// <param name="callback"></param>
+		public abstract void UnregisterOutputSigChangeCallback(uint number, eSigType type,
+		                                                       Action<SigCallbackManager, SigInfoEventArgs> callback);
+
+		/// <summary>
+		/// Sends the serial data to the panel.
+		/// </summary>
+		/// <param name="number"></param>
+		/// <param name="text"></param>
+		public abstract void SendInputSerial(uint number, string text);
+
+		/// <summary>
+		/// Sends the analog data to the panel.
+		/// </summary>
+		/// <param name="number"></param>
+		/// <param name="value"></param>
+		public abstract void SendInputAnalog(uint number, ushort value);
+
+		/// <summary>
+		/// Sends the digital data to the panel.
+		/// </summary>
+		/// <param name="number"></param>
+		/// <param name="value"></param>
+		public abstract void SendInputDigital(uint number, bool value);
+	}
+}
---
+++ ICD.Connect.Panels/Controls/SigControl.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using ICD.Common.Utils.Extensions;
+using ICD.Connect.Panels.Devices;
+using ICD.Connect.Panels.EventArguments;
+using ICD.Connect.Protocol.Sigs;
+
+namespace ICD.Connect.Panels.Controls
+{
+	public sealed class SigControl : AbstractSigControl<ISigDeviceBase>
+	{
+		/// <summary>
+		/// Raised when the user interacts with the panel.
+		/// </summary>
+		public override event EventHandler<SigInfoEventArgs> OnAnyOutput;
+
+		/// <summary>
+		/// Gets the time that the user last interacted with the panel.
+		/// </summary>
+		public override DateTime? LastOutput { get { return Parent.LastOutput; } }
+
+		/// <summary>
+		/// Constructor.
+		/// </summary>
+		/// <param name="parent"></param>
+		/// <param name="id"></param>
+		public SigControl(ISigDeviceBase parent, int id)
+			: base(parent, id)
+		{
+			Parent.OnAnyOutput += ParentOnAnyOutput;
+		}
+
+		/// <summary>
+		/// Override to release resources.
+		/// </summary>
+		/// <param name="disposing"></param>
+		protected override void DisposeFinal(bool disposing)
+		{
+			OnAnyOutput = null;
+
+			base.DisposeFinal(disposing);
+
+			Parent.OnAnyOutput -= OnAnyOutput;
+		}
+
+		/// <summary>
+		/// Clears the assigned input sig values.
+		/// </summary>
+		public override void Clear()
+		{
+			Parent.Clear();
+		}
+
+		/// <summary>
+		/// Registers the callback for output sig change events.
+		/// </summary>
+		/// <param name="number"></param>
+		/// <param name="type"></param>
+		/// <param name="callback"></param>
+		public override void RegisterOutputSigChangeCallback(uint number, eSigType type, Action<SigCallbackManager, SigInfoEventArgs> callback)
+		{
+			Parent.RegisterOutputSigChangeCallback(number, type, callback);
+		}
+
+		/// <summary>
+		/// Unregisters the callback for output sig change events.
+		/// </summary>
+		/// <param name="number"></param>
+		/// <param name="type"></param>
+		/// <param name="callback"></param>
+		public override void UnregisterOutputSigChangeCallback(uint number, eSigType type, Action<SigCallbackManager, SigInfoEventArgs> callback)
+		{
+			Parent.UnregisterOutputSigChangeCallback(number, type, callback);
+		}
+
+		/// <summary>
+		/// Sends the serial data to the panel.
+		/// </summary>
+		/// <param name="number"></param>
+		/// <param name="text"></param>
+		public override void SendInputSerial(uint number, string text)
+		{
+			Parent.SendInputSerial(number, text);
+		}
+
+		/// <summary>
+		/// Sends the analog data to the panel.
+		/// </summary>
+		/// <param name="number"></param>
+		/// <param name="value"></param>
+		public override void SendInputAnalog(uint number, ushort value)
+		{
+			Parent.SendInputAnalog(number, value);
+		}
+
+		/// <summary>
+		/// Sends the digital data to the panel.
+		/// </summary>
+		/// <param name="number"></param>
+		/// <param name="value"></param>
+		public override void SendInputDigital(uint number, bool value)
+		{
+			Parent.SendInputDigital(number, value);
+		}
+
+		/// <summary>
+		/// Called when the parent device raises any output.
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="sigInfoEventArgs"></param>
+		private void ParentOnAnyOutput(object sender, SigInfoEventArgs sigInfoEventArgs)
+		{
+			OnAnyOutput.Raise(this, new SigInfoEventArgs(sigInfoEventArgs));
+		}
+	}
+}
