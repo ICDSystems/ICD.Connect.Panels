fe49b019d6ee71e6d18666d990197e3def65310a
Chris Cameron <chris.cameron@wedoresi.com>
2018-09-12 19:04:23 -0400

    feat: Adding OnButtonStateChange event to touchscreens

--- ICD.Connect.Panels.Crestron/Controls/TouchScreens/IThreeSeriesTouchScreenControl.cs
+++ ICD.Connect.Panels.Crestron/Controls/TouchScreens/IThreeSeriesTouchScreenControl.cs
@@ -1,8 +1,14 @@
-﻿using ICD.Connect.Panels.Controls;
+﻿using System;
+using ICD.Connect.Misc.Keypads;
+using ICD.Connect.Panels.Controls;
 
 namespace ICD.Connect.Panels.Crestron.Controls.TouchScreens
 {
 	public interface IThreeSeriesTouchScreenControl : IPanelControl
 	{
+		/// <summary>
+		/// Raised when a button state changes.
+		/// </summary>
+		event EventHandler<KeypadButtonPressedEventArgs> OnButtonStateChange;
 	}
 }
--- ICD.Connect.Panels.CrestronPro/Controls/TouchScreens/AbstractThreeSeriesTouchScreenControl.cs
+++ ICD.Connect.Panels.CrestronPro/Controls/TouchScreens/AbstractThreeSeriesTouchScreenControl.cs
@@ -1,4 +1,6 @@
-﻿using ICD.Connect.Devices;
+﻿using System;
+using ICD.Connect.Devices;
+using ICD.Connect.Misc.Keypads;
 using ICD.Connect.Panels.Controls;
 using ICD.Connect.Panels.Crestron.Controls.TouchScreens;
 
@@ -7,6 +9,11 @@ namespace ICD.Connect.Panels.CrestronPro.Controls.TouchScreens
 	public abstract class AbstractThreeSeriesTouchScreenControl<TParent> : AbstractPanelControl<TParent>, IThreeSeriesTouchScreenControl
 		where TParent : IDeviceBase
 	{
+		/// <summary>
+		/// Raised when a button state changes.
+		/// </summary>
+		public abstract event EventHandler<KeypadButtonPressedEventArgs> OnButtonStateChange;
+
 		/// <summary>
 		/// Constructor.
 		/// </summary>
