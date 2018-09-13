75314cc636932bf8e13c2c43b6276aab74ec3fa4
Chris Cameron <chris.cameron@wedoresi.com>
2018-09-13 14:20:37 -0400

    feat: Adding class describing MPC3 201 button ids

--- ICD.Connect.Panels.Crestron/Controls/TouchScreens/IMPC3x201TouchScreenControl.cs
+++ ICD.Connect.Panels.Crestron/Controls/TouchScreens/IMPC3x201TouchScreenControl.cs
@@ -3,4 +3,19 @@
 	public interface IMPC3x201TouchScreenControl : IMPC3x101TouchScreenControl
 	{
 	}
+
+	public static class MPC3x201TouchScreenButtons
+	{
+		public const uint BUTTON_MUTE = 1;
+		public const uint BUTTON_VOLUME_DOWN = 2;
+		public const uint BUTTON_VOLUME_UP = 3;
+		public const uint BUTTON_POWER = 4;
+
+		public const uint BUTTON_ACTION_1 = 5;
+		public const uint BUTTON_ACTION_2 = 6;
+		public const uint BUTTON_ACTION_3 = 7;
+		public const uint BUTTON_ACTION_4 = 8;
+		public const uint BUTTON_ACTION_5 = 9;
+		public const uint BUTTON_ACTION_6 = 10;
+	}
 }
