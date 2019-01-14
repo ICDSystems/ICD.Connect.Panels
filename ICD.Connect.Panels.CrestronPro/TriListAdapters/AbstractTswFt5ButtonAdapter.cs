69ba92711683fef267113400876d5aa88a63bf67
Chris Cameron <chris.cameron@wedoresi.com>
2019-01-14 16:09:36 -0500

    refactor: Tidying

--- ICD.Connect.Panels.CrestronPro/TriListAdapters/AbstractTswFt5ButtonAdapter.cs
+++ ICD.Connect.Panels.CrestronPro/TriListAdapters/AbstractTswFt5ButtonAdapter.cs
@@ -1,6 +1,5 @@
 ﻿using ICD.Common.Properties;
 using ICD.Common.Utils.Xml;
-using ICD.Connect.Conferencing.Conferences;
 using ICD.Connect.Conferencing.Controls.Dialing;
 using ICD.Connect.Devices.Controls;
 using ICD.Connect.Settings;
