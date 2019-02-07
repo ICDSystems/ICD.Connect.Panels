297b076608c88673a80d02ab4dd550945750960a
Chris Cameron <chris.cameron@wedoresi.com>
2019-02-07 15:32:56 -0500

    chore: Updating AssemblyInfo

--- ICD.Connect.Panels/Properties/AssemblyInfo.cs
+++ ICD.Connect.Panels/Properties/AssemblyInfo.cs
@@ -3,5 +3,5 @@ using System.Reflection;
 [assembly: AssemblyTitle("ICD.Connect.Panels")]
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Connect.Panels")]
-[assembly: AssemblyCopyright("Copyright © ICD Systems 2018")]
+[assembly: AssemblyCopyright("Copyright © ICD Systems 2019")]
 [assembly: AssemblyVersion("7.0.0.0")]
