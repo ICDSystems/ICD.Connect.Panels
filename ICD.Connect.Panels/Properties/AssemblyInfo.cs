176a21b894daf08d43c53ce3b3038fe4a53f0b14
Chris Cameron <chris.cameron@wedoresi.com>
2019-05-31 11:14:49 -0400

    chore: Incrementing patch version

--- ICD.Connect.Panels/Properties/AssemblyInfo.cs
+++ ICD.Connect.Panels/Properties/AssemblyInfo.cs
@@ -4,4 +4,4 @@ using System.Reflection;
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Connect.Panels")]
 [assembly: AssemblyCopyright("Copyright © ICD Systems 2019")]
-[assembly: AssemblyVersion("6.2.2.0")]
+[assembly: AssemblyVersion("6.2.3.0")]
