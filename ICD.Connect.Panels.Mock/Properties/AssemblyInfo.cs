7b53a42d161fda6b4be3d768b5ffa04c63b70a71
Chris Cameron <chris.cameron@wedoresi.com>
2019-08-05 13:24:24 -0400

    chore: Updating changelog, incrementing minor version

--- ICD.Connect.Panels.Mock/Properties/AssemblyInfo.cs
+++ ICD.Connect.Panels.Mock/Properties/AssemblyInfo.cs
@@ -4,4 +4,4 @@ using System.Reflection;
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Connect.Panels.Mock")]
 [assembly: AssemblyCopyright("Copyright © ICD Systems 2019")]
-[assembly: AssemblyVersion("6.4.0.0")]
+[assembly: AssemblyVersion("6.5.0.0")]
