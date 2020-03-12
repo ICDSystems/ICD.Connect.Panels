e60f4355be9fcb6dbb264629e7a268900af15e7d
Austin Noska <austin.noska@profound-tech.com>
2020-03-12 16:26:42 -0400

    fix: Fixing NET standard build

--- ICD.Connect.Panels.CrestronPro/TriListAdapters/Dge/AbstractDge100Adapter.cs
+++ ICD.Connect.Panels.CrestronPro/TriListAdapters/Dge/AbstractDge100Adapter.cs
@@ -1,10 +1,8 @@
-﻿using Crestron.SimplSharpPro.UI;
-
-namespace ICD.Connect.Panels.CrestronPro.TriListAdapters.Dge
+﻿namespace ICD.Connect.Panels.CrestronPro.TriListAdapters.Dge
 {
 #if SIMPLSHARP
 	public abstract class AbstractDge100Adapter<TPanel, TSettings> : AbstractTriListAdapter<TPanel, TSettings>, IDge100Adapter
-		where TPanel : Dge100
+		where TPanel : global::Crestron.SimplSharpPro.UI.Dge100
 #else
 	public abstract class AbstractDge100Adapter<TSettings> : AbstractTriListAdapter<TSettings>, IDge100Adapter
 #endif
