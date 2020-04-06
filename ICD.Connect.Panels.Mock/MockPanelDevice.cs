0e6b43ccb5bccc9a736e579310e3625fd7215f2a
Drew Tingen <drew.tingen@icdsystems.com>
2020-04-06 18:12:06 -0400

    feat: MockPanelDevice implements IMockDevice

--- ICD.Connect.Panels.Mock/MockPanelDevice.cs
+++ ICD.Connect.Panels.Mock/MockPanelDevice.cs
@@ -1,24 +1,32 @@
-﻿using ICD.Common.Properties;
+﻿using System.Collections.Generic;
+using ICD.Common.Properties;
+using ICD.Connect.API.Commands;
+using ICD.Connect.API.Nodes;
+using ICD.Connect.Devices.Mock;
 using ICD.Connect.Panels.Devices;
 using ICD.Connect.Panels.EventArguments;
 using ICD.Connect.Panels.SigCollections;
 using ICD.Connect.Panels.SmartObjectCollections;
 using ICD.Connect.Panels.SmartObjects;
 using ICD.Connect.Protocol.Sigs;
+using ICD.Connect.Settings;
 
 namespace ICD.Connect.Panels.Mock
 {
 	/// <summary>
 	/// MockPanelDevice provides a way for us to test UI code without using a panel.
 	/// </summary>
-	public sealed class MockPanelDevice : AbstractPanelDevice<MockPanelDeviceSettings>
+	public sealed class MockPanelDevice : AbstractPanelDevice<MockPanelDeviceSettings>, IMockDevice
 	{
 		private readonly IDeviceBooleanInputCollection m_BooleanInput;
 		private readonly IDeviceUShortInputCollection m_UShortInput;
 		private readonly IDeviceStringInputCollection m_StringInput;
 		private readonly ISmartObjectCollection m_SmartObjects;
+		private bool m_IsOnline;
 
 		#region Properties
+
+		public bool DefaultOffline { get; set; }
 
 		/// <summary>
 		/// Collection of Boolean Inputs sent to the device.
@@ -82,10 +90,16 @@ namespace ICD.Connect.Panels.Mock
 		/// <returns></returns>
 		protected override bool GetIsOnlineStatus()
 		{
-			return true;
+			return m_IsOnline;
 		}
 
-	    #region SmartObject Callbacks
+		public void SetIsOnlineState(bool isOnline)
+		{
+			m_IsOnline = isOnline;
+			UpdateCachedOnlineStatus();
+		}
+
+		#region SmartObject Callbacks
 
 	    private void Subscribe(ISmartObjectCollection smartObjects)
 	    {
@@ -130,5 +144,75 @@ namespace ICD.Connect.Panels.Mock
 	    }
 
 	    #endregion
-    }
+
+		#region Settings
+
+		/// <summary>
+		/// Override to apply settings to the instance.
+		/// </summary>
+		/// <param name="settings"></param>
+		/// <param name="factory"></param>
+		protected override void ApplySettingsFinal(MockPanelDeviceSettings settings, IDeviceFactory factory)
+		{
+			base.ApplySettingsFinal(settings, factory);
+
+			MockDeviceHelper.ApplySettings(this, settings);
+		}
+
+		/// <summary>
+		/// Override to apply properties to the settings instance.
+		/// </summary>
+		/// <param name="settings"></param>
+		protected override void CopySettingsFinal(MockPanelDeviceSettings settings)
+		{
+			base.CopySettingsFinal(settings);
+
+			MockDeviceHelper.CopySettings(this,settings);
+		}
+
+		/// <summary>
+		/// Override to clear the instance settings.
+		/// </summary>
+		protected override void ClearSettingsFinal()
+		{
+			base.ClearSettingsFinal();
+
+			MockDeviceHelper.ClearSettings(this);
+		}
+
+		#endregion
+
+		#region Console
+
+		/// <summary>
+		/// Calls the delegate for each console status item.
+		/// </summary>
+		/// <param name="addRow"></param>
+		public override void BuildConsoleStatus(AddStatusRowDelegate addRow)
+		{
+			base.BuildConsoleStatus(addRow);
+
+			MockDeviceHelper.BuildConsoleStatus(this, addRow);
+		}
+
+		/// <summary>
+		/// Gets the child console commands.
+		/// </summary>
+		/// <returns></returns>
+		public override IEnumerable<IConsoleCommand> GetConsoleCommands()
+		{
+			foreach (IConsoleCommand command in GetBaseConsoleCommands())
+				yield return command;
+
+			foreach (IConsoleCommand command in MockDeviceHelper.GetConsoleCommands(this))
+				yield return command;
+		}
+
+		private IEnumerable<IConsoleCommand> GetBaseConsoleCommands()
+		{
+			return base.GetConsoleCommands();
+		}
+
+		#endregion
+	}
 }
--- ICD.Connect.Panels.Mock/MockPanelDeviceSettings.cs
+++ ICD.Connect.Panels.Mock/MockPanelDeviceSettings.cs
@@ -1,10 +1,35 @@
+using ICD.Common.Utils.Xml;
+using ICD.Connect.Devices.Mock;
 using ICD.Connect.Panels.Devices;
 using ICD.Connect.Settings.Attributes;
 
 namespace ICD.Connect.Panels.Mock
 {
 	[KrangSettings("MockPanel", typeof(MockPanelDevice))]
-	public sealed class MockPanelDeviceSettings : AbstractPanelDeviceSettings
+	public sealed class MockPanelDeviceSettings : AbstractPanelDeviceSettings, IMockDeviceSettings
 	{
+		public bool DefaultOffline { get; set; }
+
+		/// <summary>
+		/// Writes property elements to xml.
+		/// </summary>
+		/// <param name="writer"></param>
+		protected override void WriteElements(IcdXmlTextWriter writer)
+		{
+			base.WriteElements(writer);
+
+			MockDeviceSettingsHelper.WriteElements(this, writer);
+		}
+
+		/// <summary>
+		/// Updates the settings from xml.
+		/// </summary>
+		/// <param name="xml"></param>
+		public override void ParseXml(string xml)
+		{
+			base.ParseXml(xml);
+
+			MockDeviceSettingsHelper.ParseXml(this, xml);
+		}
 	}
 }
