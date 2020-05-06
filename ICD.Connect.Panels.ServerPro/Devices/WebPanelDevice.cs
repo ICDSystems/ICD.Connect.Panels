d5a8149dcdfd713b929d7d211dfd1c204493e04d
Chris Cameron <chris.cameron@wedoresi.com>
2020-05-06 10:15:47 -0400

    feat: Initial commit of WebPanelDevice

---
+++ ICD.Connect.Panels.ServerPro/Devices/WebPanelDevice.cs
@@ -0,0 +1,15 @@
+﻿using ICD.Connect.Panels.Server;
+using ICD.Connect.Protocol.NetworkPro.Ports.WebSockets;
+using ICD.Connect.Settings.Attributes;
+
+namespace ICD.Connect.Panels.ServerPro.Devices
+{
+	public sealed class WebPanelDevice : AbstractPanelServerDevice<IcdWebSocketServer, WebPanelDeviceSettings>
+	{
+	}
+
+	[KrangSettings("WebPanelDevice", typeof(WebPanelDevice))]
+	public sealed class WebPanelDeviceSettings : AbstractPanelServerDeviceSettings
+	{
+	}
+}
---
+++ ICD.Connect.Panels.ServerPro/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+using System.Reflection;
+
+[assembly: AssemblyTitle("ICD.Connect.Panels.ServerPro")]
+[assembly: AssemblyCompany("ICD Systems")]
+[assembly: AssemblyProduct("ICD.Connect.Panels.ServerPro")]
+[assembly: AssemblyCopyright("Copyright © ICD Systems 2020")]
+[assembly: AssemblyVersion("7.3.0.0")]
