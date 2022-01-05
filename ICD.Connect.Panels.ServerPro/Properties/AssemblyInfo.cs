58f4a8816da08ffa9dd075e7d856d5b02773299c
Chris Cameron <chris.cameron@icdsystems.com>
2022-01-05 16:27:18 -0500

    chore: Updating AssemblyInfo year

--- ICD.Connect.Panels.ServerPro/Properties/AssemblyInfo.cs
+++ ICD.Connect.Panels.ServerPro/Properties/AssemblyInfo.cs
@@ -3,5 +3,5 @@ using System.Reflection;
 [assembly: AssemblyTitle("ICD.Connect.Panels.ServerPro")]
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Connect.Panels.ServerPro")]
-[assembly: AssemblyCopyright("Copyright © ICD Systems 2021")]
+[assembly: AssemblyCopyright("Copyright © ICD Systems 2022")]
 [assembly: AssemblyVersion("9.2.1.0")]
--- ICD.Connect.Panels/Properties/AssemblyInfo.cs
+++ ICD.Connect.Panels/Properties/AssemblyInfo.cs
@@ -3,5 +3,5 @@ using System.Reflection;
 [assembly: AssemblyTitle("ICD.Connect.Panels")]
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Connect.Panels")]
-[assembly: AssemblyCopyright("Copyright © ICD Systems 2021")]
+[assembly: AssemblyCopyright("Copyright © ICD Systems 2022")]
 [assembly: AssemblyVersion("9.2.1.0")]
